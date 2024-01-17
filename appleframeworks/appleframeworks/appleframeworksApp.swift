9aa7069cbf5608e929b23402d91cbcb1222dfeb8
gorav-code <gorav.code@outlook.com>
2024-01-17 13:24:15 +0530

    initial commit

---
+++ appleframeworks/appleframeworks/appleframeworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleframeworksApp.swift
+//  appleframeworks
+//
+//  Created by Gaurav Kundalwal on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct appleframeworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
